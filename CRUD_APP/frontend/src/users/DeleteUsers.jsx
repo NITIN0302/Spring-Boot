bf9e76fecd376ff5fcd0be4268842668324892f8
Nitin Srivastva <nitinsrivastava2all@gmail.com>
2024-11-18 01:38:03 +0530

    All CRUD operation

--- CRUD_APP/frontend/src/users/DeleteUsers.jsx
+++ CRUD_APP/frontend/src/users/DeleteUsers.jsx
@@ -1,21 +1,27 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import Modal from "../Modal";
 
 const DeleteUsers = () => {
   const [users, setUsers] = useState([]);
 
   useEffect(() => {
     getUsers();
-  }, []);
+  }, [users]);
 
   const getUsers = async () => {
     const result = await axios.get("http://localhost:8080/users");
     setUsers(result.data);
   };
 
-  const DeleteUsers = (id) =>{
-
-  }
+  const DeleteUsers = async (id) => {
+    const value = prompt("Are you sure you want to delete this user (Type Yes/No)");
+    if(value.toUpperCase() == "YES"){
+        const result = await axios.delete(`http://localhost:8080/users/${id}`);
+        console.log(result);
+    }
+    getUsers();
+  };
 
   return (
     <div className="flex flex-wrap justify-center">
@@ -27,7 +33,10 @@ const DeleteUsers = () => {
         <div className="w-[10%] border-black text-blue-500"></div>
       </div>
       {users.map((ele) => (
-        <div key={ele.id} className="w-[70%] flex flex-wrap justify-around p-2 bg-custom-bg1 overflow-auto ">
+        <div
+          key={ele.id}
+          className="w-[70%] flex flex-wrap justify-around p-2 bg-custom-bg1 overflow-auto "
+        >
           <div className="w-[20%] border-black text-white">{ele.id}</div>
           <div className="w-[20%] border-black text-white">{ele.userName}</div>
           <div className="w-[20%] border-black text-white">{ele.name}</div>
@@ -37,6 +46,7 @@ const DeleteUsers = () => {
           <div className="w-[10%] border-black">
             <button
               onClick={() => {
+                DeleteUsers(ele.id);
               }}
               className="bg-blue-600 px-4 py-1 rounded-md text-gray-300"
             >
