b7d64c6a1e795179a87a46e5b2f7d5a76567dd3c
Nitin Srivastva <nitinsrivastava2all@gmail.com>
2024-11-25 22:41:25 +0530

    QuizApp

---
+++ QuizzApp/quizeapp/src/test/java/com/project/quizeapp/QuizeappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.quizeapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizeappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
